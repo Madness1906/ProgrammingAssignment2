d0c4480730689ed00fab41a4ef9a5c6faf573cb7
Madness1906 <rjcastillo19@gmail.com>
2020-10-05 21:21:53 -0500

    Functions to create List_matrix from simple matrix and to solve the inverse of List_matrix. Second attempt.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## function to create the matrix with its cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<-NULL
+  set<-function(y){
+    x<<-y
+    inv<<-NULL
+  }
+  get<-function() x
+  setInv<-function(inversa) inv <<- inversa
+  getInv<-function() inv
+  list(set = set,get = get,setInv=setInv,getInv=getInv)
 }
 
-
-## Write a short comment describing this function
+## funcion para generar la inversa y guardarla en cache o recuperarla
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv<-x$getInv()
+  if(!is.null(inv)){
+    message("fetching the cached data")
+    return(inv)
+  }
+  data<-x$get()
+  inv<-solve(data)
+  x$setInv(inv)
+  inv
 }
